54d152f0b21668191a9343798be7f87fe6bad53a
faezeh azadi <faezeh.azadi31@gmail.com>
2024-02-21 23:32:07 -0800

    60n

--- assignment 60/face_verification/face_verification.py
+++ assignment 60/face_verification/face_verification.py
@@ -15,14 +15,21 @@ import argparse
 app = FaceAnalysis(providers=['CPUExecutionProvider'], name='buffalo_s')
 app.prepare(ctx_id=0, det_size=(640, 640))
 
-image_1 = cv2.imread("/content/r1.jpg")
+image_1 = cv2.imread("/content/face_bank/robert pattinson/r1.jpg")
 image_1 = cv2.cvtColor(image_1, cv2.COLOR_BGR2RGB)
 
 result = app.get(image_1)
 embeding1 = result[0]["embedding"]
 
-image_2 = cv2.imread("/content/r2.jpg")
+image_2 = cv2.imread("/content/face_bank/robert pattinson/r2.jpg")
 image_2 = cv2.cvtColor(image_2, cv2.COLOR_BGR2RGB)
 
 result2 = app.get(image_2)
 embeding2 = result2[0]["embedding"]
+
+if np.sqrt(np.sum((embeding1 - embeding2)**2))<25:
+    out = pyfiglet.figlet_format("same")
+    print(out)
+else:
+    out = pyfiglet.figlet_format("Different")
+    print(out)
